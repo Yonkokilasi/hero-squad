cc929b9110c194e19bd53db4e25c7a91cc0fe961
Yonkokilasi <yonkokilasi@gmail.com>
2017-05-05 15:48:11 +0300

    Integrated heroku database

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,19 +5,21 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+  ProcessBuilder processBuilder = new ProcessBuilder();
+  if (processBuilder.environment().get("PORT") != null) {
+    return Integer.parseInt(processBuilder.environment().get("PORT"));
+  }
+  return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+}
   public static void main(String[] args) {
       staticFileLocation("/public");
       String layout = "templates/layout.vtl";
 
-      ProcessBuilder process = new ProcessBuilder();
-      Integer port;
-      if (process.environment().get("PORT") != null) {
-          port = Integer.parseInt(process.environment().get("PORT"));
-      } else {
-          port = 4567;
-      }
+    port(getHerokuAssignedPort());
+    staticFileLocation("/public");
 
-     setPort(port);
+     
 
       get("/", (request, response) -> {
           Map<String, Object> model = new HashMap<String, Object> ();
--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,13 @@
+// import org.sql2o.*;
+//
+// public class DB {
+//   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hero_squad", "bubbles", "enter");
+// }
+
+
+
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hero_squad", "bubbles", "enter");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-23-23-220-163.compute-1.amazonaws.com:5432/d5ird3ra82dafq", "vnwnxufyxuqypr", "ba31c76b54e4b4ff1295720353772702a99473833d8bcd68ee186436b9624c63");
 }
